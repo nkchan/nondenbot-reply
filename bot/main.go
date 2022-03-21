32ab9a6b7ba81213118be535082670e3c5da0451
Kaoru Nagashima <nkchan0228@gmail.com>
2022-03-21 22:00:49 +0900

    get line info

--- bot/main.go
+++ bot/main.go
@@ -4,18 +4,14 @@ import (
 	"bytes"
 	"context"
 	"encoding/json"
+	"log"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
-// Response is of type APIGatewayProxyResponse since we're leveraging the
-// AWS Lambda Proxy Request functionality (default behavior)
-//
-// https://serverless.com/framework/docs/providers/aws/events/apigateway/#lambda-proxy-integration
 type Response events.APIGatewayProxyResponse
 
-// Handler is our lambda handler invoked by the `lambda.Start` function call
 func Handler(ctx context.Context) (Response, error) {
 	var buf bytes.Buffer
 
@@ -26,6 +22,8 @@ func Handler(ctx context.Context) (Response, error) {
 		return Response{StatusCode: 404}, err
 	}
 	json.HTMLEscape(&buf, body)
+
+	log.Println(body)
 
 	resp := Response{
 		StatusCode:      200,
