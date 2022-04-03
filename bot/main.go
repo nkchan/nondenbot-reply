5e616292a4ebd0afc0aaaae259d0285f82966798
Kaoru Nagashima <nkchan0228@gmail.com>
2022-04-03 19:18:09 +0900

    test

--- bot/main.go
+++ bot/main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
-	"errors"
+	"crypto/hmac"
+	"crypto/sha256"
+	"encoding/base64"
 	"fmt"
+	"log"
+	"net/http"
 	"os"
 
 	"github.com/aws/aws-lambda-go/events"
@@ -13,25 +17,36 @@ import (
 type Response events.APIGatewayProxyResponse
 
 func Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	line := Line{}
-	err := line.New(
-		os.Getenv("LINE_BOT_CHANNEL_SECRET"),
-		os.Getenv("LINE_BOT_CHANNEL_TOKEN"))
-	if err != nil {
-		fmt.Println(err)
+
+	log.Print(request.Headers)
+	log.Print(request.Body)
+
+	if !validateSignature(os.Getenv("LINE_CHANNEL_SECRET"), request.Headers["X-Line-Signature"], []byte(request.Body)) {
+		return events.APIGatewayProxyResponse{
+			StatusCode: http.StatusBadRequest,
+			Body:       fmt.Sprintf(`{"message":"%s"}`+"\n", linebot.ErrInvalidSignature.Error()),
+		}, nil
 	}
-	eve, err := ParseRequest(line.ChannelSecret, request)
+
+	return events.APIGatewayProxyResponse{
+		StatusCode: 200,
+	}, nil
+
+}
+
+func validateSignature(channelSecret string, signature string, body []byte) bool {
+	decoded, err := base64.StdEncoding.DecodeString(signature)
 	if err != nil {
-		status := 200
-		if err == linebot.ErrInvalidSignature {
-			status = 400
-		} else {
-			status = 500
-		}
-		return events.APIGatewayProxyResponse{StatusCode: status}, errors.New("Bat Request")
+		return false
 	}
-	line.EventRouter(eve)
-	return events.APIGatewayProxyResponse{Body: request.Body, StatusCode: 200}, nil
+
+	hash := hmac.New(sha256.New, []byte(channelSecret))
+	_, err = hash.Write(body)
+	if err != nil {
+		return false
+	}
+
+	return hmac.Equal(decoded, hash.Sum(nil))
 }
 
 func main() {
--- go.mod
+++ go.mod
@@ -2,9 +2,9 @@ module github.com/nkchan/nondenbot-reply/bot
 
 go 1.17
 
-require github.com/aws/aws-lambda-go v1.28.0
-
 require (
-	github.com/line/line-bot-sdk-go v7.8.0+incompatible // indirect
-	github.com/line/line-bot-sdk-go/v7 v7.14.0 // indirect
+	github.com/aws/aws-lambda-go v1.29.0
+	github.com/line/line-bot-sdk-go/v7 v7.14.0
 )
+
+require github.com/line/line-bot-sdk-go v7.8.0+incompatible // indirect
