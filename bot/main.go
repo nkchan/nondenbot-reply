8d8a8029b9cfd25d5fa794891fc747c2541e1617
Kaoru Nagashima <nkchan0228@gmail.com>
2022-04-03 19:27:05 +0900

    test

--- bot/main.go
+++ bot/main.go
@@ -4,14 +4,10 @@ import (
 	"crypto/hmac"
 	"crypto/sha256"
 	"encoding/base64"
-	"fmt"
 	"log"
-	"net/http"
-	"os"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
-	"github.com/line/line-bot-sdk-go/v7/linebot"
 )
 
 type Response events.APIGatewayProxyResponse
@@ -20,13 +16,6 @@ func Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyRespo
 
 	log.Print(request.Headers)
 	log.Print(request.Body)
-
-	if !validateSignature(os.Getenv("LINE_CHANNEL_SECRET"), request.Headers["X-Line-Signature"], []byte(request.Body)) {
-		return events.APIGatewayProxyResponse{
-			StatusCode: http.StatusBadRequest,
-			Body:       fmt.Sprintf(`{"message":"%s"}`+"\n", linebot.ErrInvalidSignature.Error()),
-		}, nil
-	}
 
 	return events.APIGatewayProxyResponse{
 		Body:       "",
