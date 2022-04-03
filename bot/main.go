976c7bda5e40869485def3060e1fda46d07cbd71
Kaoru Nagashima <nkchan0228@gmail.com>
2022-04-03 19:46:48 +0900

    test

--- bot/main.go
+++ bot/main.go
@@ -4,27 +4,51 @@ import (
 	"crypto/hmac"
 	"crypto/sha256"
 	"encoding/base64"
+	"encoding/json"
 	"log"
+	"os"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/line/line-bot-sdk-go/linebot"
 )
 
 type Response events.APIGatewayProxyResponse
 
 func Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 
-	log.Print(request.Headers)
-	log.Print(request.Body)
+	bot, err := linebot.New(
+		os.Getenv("LINE_CHANNE_TOKEN"),
+		os.Getenv("LINE_CHANNEL_SECRET"),
+	)
+	// エラーに値があればログに出力し終了する
+	if err != nil {
+		log.Fatal(err)
+	}
+	result := "hoge"
 
-	return events.APIGatewayProxyResponse{
-		Body:       "",
-		StatusCode: 200,
-	}, nil
+	// テキストメッセージを生成する
+	message := linebot.NewTextMessage(result)
+	// テキストメッセージを友達登録しているユーザー全員に配信する
+	if _, err := bot.BroadcastMessage(message).Do(); err != nil {
+		log.Fatal(err)
+	}
 
 }
+func ParseRequest(channelSecret string, r events.APIGatewayProxyRequest) ([]*linebot.Event, error) {
+	if !ValidateSignature(channelSecret, r.Headers["x-line-signature"], []byte(r.Body)) {
+		return nil, linebot.ErrInvalidSignature
+	}
+	request := &struct {
+		Events []*linebot.Event `json:"events"`
+	}{}
+	if err := json.Unmarshal([]byte(r.Body), request); err != nil {
+		return nil, err
+	}
+	return request.Events, nil
+}
 
-func validateSignature(channelSecret string, signature string, body []byte) bool {
+func ValidateSignature(channelSecret string, signature string, body []byte) bool {
 	decoded, err := base64.StdEncoding.DecodeString(signature)
 	if err != nil {
 		return false
